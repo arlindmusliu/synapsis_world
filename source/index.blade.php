e964a9f3d2d818b698fc90ffbf13c2753d114baa
Duti <zaimidaut@gmail.com>
2020-03-16 00:27:57 +0100

    index making

--- source/index.blade.php
+++ source/index.blade.php
@@ -20,19 +20,87 @@
             <h2 class="text-4xl">
                 WELCOME TO MUSTANG
             </h2>
-            <p class="mt-16 text-justify offset-1">
-                Your heart is true you’re a pal and a confidant. Got kind of tired packin’ and unpackin’ – town to town
-                and up and down the dial. That this group would somehow form a family that’s the way we all became the
-                Brady Bunch.
-            </p>
-            <p class="mt-12 text-justify offset-1">
-                That this group would somehow form a family that’s the way we all became the Brady Bunch. You wanna be
-                where you can see all the same.
-            </p>
-            <p class="my-12 text-justify offset-1">
-                That this group would somehow form a family that’s the way we all became the Brady Bunch. You wanna be
-                where you can see all the same.
-            </p>
+            <div class="offset-1">
+                <p class="mt-16 text-justify ">
+                    Your heart is true you’re a pal and a confidant. Got kind of tired packin’ and unpackin’ – town to
+                    town
+                    and up and down the dial. That this group would somehow form a family that’s the way we all became
+                    the
+                    Brady Bunch.
+                </p>
+                <p class="mt-12 text-justify">
+                    That this group would somehow form a family that’s the way we all became the Brady Bunch. You wanna
+                    be
+                    where you can see all the same.
+                </p>
+                <p class="my-12 text-justify ">
+                    That this group would somehow form a family that’s the way we all became the Brady Bunch. You wanna
+                    be
+                    where you can see all the same.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="bg-black">
+    <div class="container">
+        <div class="flex-row">
+            <h2 class="text-white text-center text-4xl">
+                SERVICES THAT WE PROVIDE
+            </h2>
+        </div>
+        <div class="flex flex-row items-center text-center mt-24">
+            <div class="col-5">
+                <img src="/assets/images/service1-img.png" alt="service1-img" class="w-full h-auto">
+            </div>
+            <div class="col-5">
+                <h2 class="text-white text-xl"> WE ARE KNOWN FOR UNIQUE & CREATIVE DESIGN </h2>
+                <p class="mt-24 text-white">
+                    He’s a demon on wheels. Their house is a museum where people come to see ‘em. They really are a
+                    scream the Addams Family. work to do. You bet your life Speed Racer he will see it.
+                </p>
+            </div>
+        </div>
+        <div class="flex flex-row items-center">
+            <div class="col-5">
+                <h2 class="text-white text-xl"> WE ARE KNOWN FOR UNIQUE & CREATIVE DESIGN </h2>
+                <p class="mt-24 text-white">
+                    He’s a demon on wheels. Their house is a museum where people come to see ‘em. They really are a
+                    scream the Addams Family. work to do. You bet your life Speed Racer he will see it.
+                </p>
+            </div>
+            <div class="col-5">
+                <img src="/assets/images/service2-img.png" alt="service2-img" class="w-full h-auto">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="flex-row">
+        <h1 class="text-4xl text-center">
+            Partners
+        </h1>
+    </div>
+    <div class="flex-row">
+
+    </div>
+</div>
+<div class="container">
+    <div class="flex-row">
+        <h1 class="text-4xl text-center mt-32">
+            LATEST PROJECTS
+        </h1>
+    </div>
+    <div class="flex flex-row mt-20">
+        <div class="col-6">
+            <img src="/assets/images/projects1-img.png" class="w-full h-auto" alt="">
+        </div>
+        <div class="col-6">
+            <h2 class="text-4xl">
+                YEPTALKS
+            </h2>
+            
+
         </div>
     </div>
 </div>
